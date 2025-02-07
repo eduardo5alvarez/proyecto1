564309ca1538529c93100c18d1c8b130da7473ae
eduardo5alvarez <eduao1412@gmail.com>
2025-02-07 10:11:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? yourName = null;   
+
+            Console.WriteLine("Bienvenido a ESPAÑA");
+            Console.WriteLine("Pulse cualquier tecla para conseguir tu pasaporte");
+            Console.ReadKey();
+            Console.WriteLine("Introduce tu nombre");
+            yourName = Console.ReadLine();
+
+            Console.WriteLine($"Hola, {yourName} has sido enlistado en las fuerzas terrestres, creias que era asi de facil conseguir la nacionalidad morito?.");
+            Console.WriteLine("Pulse cualquier tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("Desterrando a jovenlandia...");
+        }
+    }
+}
